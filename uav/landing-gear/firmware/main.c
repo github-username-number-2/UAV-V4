be3b67a41ee90038d092b24eaee48ee69a75ef6c
Luke Baja-Ricketts <luekbaja@gmail.com>
2025-03-24 22:48:35 -0400

    uh

--- uav/landing-gear/firmware/main.c
+++ uav/landing-gear/firmware/main.c
@@ -1,6 +1,43 @@
-int main() {
+ /*
+ * MAIN Generated Driver File
+ * 
+ * @file main.c
+ * 
+ * @defgroup main MAIN
+ * 
+ * @brief This is the generated driver implementation file for the MAIN driver.
+ *
+ * @version MAIN Driver Version 1.0.2
+ *
+ * @version Package Version: 3.1.2
+*/
 
-    // Add your code here and press Ctrl + Shift + B to build
+#include "mcc_generated_files/system/system.h"
 
-    return 0;
+/*
+    Main application
+*/
+
+int main(void)
+{
+    SYSTEM_Initialize();
+    // If using interrupts in PIC18 High/Low Priority Mode you need to enable the Global High and Low Interrupts 
+    // If using interrupts in PIC Mid-Range Compatibility Mode you need to enable the Global and Peripheral Interrupts 
+    // Use the following macros to: 
+
+    // Enable the Global Interrupts 
+    //INTERRUPT_GlobalInterruptEnable(); 
+
+    // Disable the Global Interrupts 
+    //INTERRUPT_GlobalInterruptDisable(); 
+
+    // Enable the Peripheral Interrupts 
+    //INTERRUPT_PeripheralInterruptEnable(); 
+
+    // Disable the Peripheral Interrupts 
+    //INTERRUPT_PeripheralInterruptDisable(); 
+
+
+    while(1) {
+    }    
 }
